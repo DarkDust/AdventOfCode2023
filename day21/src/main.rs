d0e96f732a09f71a71060cb038796fb16e998ab2
Marc Haisenko <haisenko@equinux.com>
2023-12-23 14:04:41 +0100

    Day 21, unfinished part 2

---
+++ day21/src/main.rs
@@ -0,0 +1,125 @@
+use std::{collections::HashSet, time::Instant};
+
+#[derive(Debug)]
+enum Error {}
+
+struct Map {
+    rocks: HashSet<(isize, isize)>,
+    positions: HashSet<(isize, isize)>,
+    x_len: isize,
+    y_len: isize,
+}
+
+impl Map {
+    fn new(input: &str) -> Result<Map, Error> {
+        let mut rocks: HashSet<(isize, isize)> = HashSet::new();
+        let mut positions: HashSet<(isize, isize)> = HashSet::new();
+        let mut x_len = 0;
+        let mut y_len = 0;
+
+        for (y, line) in input.lines().enumerate() {
+            x_len = line.len() as isize;
+            y_len += 1;
+
+            for (x, char) in line.chars().enumerate() {
+                match char {
+                    '#' => _ = rocks.insert((x as isize, y as isize)),
+                    'S' => _ = positions.insert((x as isize, y as isize)),
+                    _ => (),
+                }
+            }
+        }
+
+        return Ok(Map {
+            rocks,
+            positions,
+            x_len,
+            y_len,
+        });
+    }
+
+    fn step<F>(&self, next_steps: F) -> Map
+    where
+        F: Fn(&Map, &mut HashSet<(isize, isize)>, &(isize, isize)),
+    {
+        let mut new_positions: HashSet<(isize, isize)> = HashSet::new();
+
+        for pos in &self.positions {
+            next_steps(self, &mut new_positions, pos);
+        }
+
+        Map {
+            rocks: self.rocks.clone(),
+            positions: new_positions,
+            x_len: self.x_len,
+            y_len: self.y_len,
+        }
+    }
+
+    fn next_steps_limited(&self, positions: &mut HashSet<(isize, isize)>, from: &(isize, isize)) {
+        let x = from.0;
+        let y = from.1;
+
+        if x > 0 {
+            self.push_step(positions, x - 1, y);
+        }
+        if x + 1 < self.x_len {
+            self.push_step(positions, x + 1, y);
+        }
+        if y > 0 {
+            self.push_step(positions, x, y - 1);
+        }
+        if y + 1 < self.y_len {
+            self.push_step(positions, x, y + 1);
+        }
+    }
+
+    fn next_steps_infinite(&self, positions: &mut HashSet<(isize, isize)>, from: &(isize, isize)) {
+        let x = from.0;
+        let y = from.1;
+
+        self.push_step(positions, x - 1, y);
+        self.push_step(positions, x + 1, y);
+        self.push_step(positions, x, y - 1);
+        self.push_step(positions, x, y + 1);
+    }
+
+    fn push_step(&self, positions: &mut HashSet<(isize, isize)>, x: isize, y: isize) {
+        let normalized_pos = (x.rem_euclid(self.x_len), y.rem_euclid(self.y_len));
+        if !self.rocks.contains(&normalized_pos) {
+            positions.insert((x, y));
+        }
+    }
+}
+
+fn part1(input: &str) -> Result<(), Error> {
+    let mut map = Map::new(input)?;
+    for _ in 0..64 {
+        map = map.step(Map::next_steps_limited);
+    }
+    println!("Part 1: {}", map.positions.len());
+    return Ok(());
+}
+
+fn part2(input: &str) -> Result<(), Error> {
+    let mut map = Map::new(input)?;
+    for _ in 0..50 {
+        map = map.step(Map::next_steps_limited);
+    }
+    println!("Part 2: I have no idea how to find this, yet.");
+    return Ok(());
+}
+
+fn main() -> Result<(), Error> {
+    let input = include_str!("../rsc/sample1.txt");
+
+    let start1 = Instant::now();
+    part1(input)?;
+    println!("Elapsed: {:.2?}\n", start1.elapsed());
+
+    let start2 = Instant::now();
+    part2(input)?;
+    println!("Elapsed: {:.2?}", start2.elapsed());
+
+    Ok(())
+}

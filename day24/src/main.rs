e01b5230022e7088d5e126fee2c5d820c01e45a3
Marc Haisenko <haisenko@equinux.com>
2023-12-25 13:45:19 +0100

    Day 24 part 2

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,3 +1,4 @@
+use rug::Float;
 use std::time::Instant;
 
 #[derive(Debug)]
@@ -5,7 +6,7 @@ enum Error {
     InvalidInput,
 }
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq)]
 struct Coord3 {
     x: f64,
     y: f64,
@@ -37,14 +38,6 @@ impl Coord3 {
             .map_err(|_| Error::InvalidInput)?;
         Ok(Coord3 { x, y, z })
     }
-
-    fn scale(&self, factor: f64) -> Coord3 {
-        Coord3 {
-            x: self.x * factor,
-            y: self.y * factor,
-            z: self.z * factor,
-        }
-    }
 }
 
 impl std::ops::Add for &Coord3 {
@@ -55,6 +48,18 @@ impl std::ops::Add for &Coord3 {
             x: self.x + rhs.x,
             y: self.y + rhs.y,
             z: self.z + rhs.z,
+        }
+    }
+}
+
+impl std::ops::Sub for &Coord3 {
+    type Output = Coord3;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Coord3 {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
         }
     }
 }
@@ -73,32 +78,154 @@ impl Hailstone {
         Ok(Hailstone { pos, vel })
     }
 
-    fn intersection_flat(&self, other: &Hailstone) -> Option<(f64, f64)> {
-        // I don't understand why, but if I used `&self.pos + &self.vel` for p2 (and same for p4),
-        // the result were too few intersections in the real input data. I suspect rounding errors.
-        let p1 = &self.pos;
-        let p2 = &self.pos + &self.vel.scale(10.0);
-        let p3 = &other.pos;
-        let p4 = &other.pos + &other.vel.scale(10.0);
+    fn _intersection(
+        p1: (f64, f64),
+        p2: (f64, f64),
+        p3: (f64, f64),
+        p4: (f64, f64),
+    ) -> Option<(f64, f64)> {
+        // I seem to be missing something about how to use Rug, the following code is very verbose
+        // and involves a lot of (probably unnecessary) clones… and it's slow.
+        let prec = 128;
+        let x1 = Float::with_val(prec, p1.0);
+        let x2 = Float::with_val(prec, p2.0);
+        let x3 = Float::with_val(prec, p3.0);
+        let x4 = Float::with_val(prec, p4.0);
+        let y1 = Float::with_val(prec, p1.1);
+        let y2 = Float::with_val(prec, p2.1);
+        let y3 = Float::with_val(prec, p3.1);
+        let y4 = Float::with_val(prec, p4.1);
 
         // https://en.wikipedia.org/wiki/Line–line_intersection#Given_two_points_on_each_line
-        // Can likely be writter in a more readable way… 🤷‍♂️
-        let x_nom = (p1.x * p2.y - p1.y * p2.x) * (p3.x - p4.x)
-            - (p1.x - p2.x) * (p3.x * p4.y - p3.y * p4.x);
-        let x_denom = (p1.x - p2.x) * (p3.y - p4.y) - (p1.y - p2.y) * (p3.x - p4.x);
-        let y_nom = (p1.x * p2.y - p1.y * p2.x) * (p3.y - p4.y)
-            - (p1.y - p2.y) * (p3.x * p4.y - p3.y * p4.x);
-        let y_denom = (p1.x - p2.x) * (p3.y - p4.y) - (p1.y - p2.y) * (p3.x - p4.x);
-        if x_denom == 0.0 || y_denom == 0.0 {
+        let denom = (x1.clone() - x2.clone()) * (y3.clone() - y4.clone())
+            - (y1.clone() - y2.clone()) * (x3.clone() - x4.clone());
+        if denom.is_zero() {
             return None;
         }
-        Some((x_nom / x_denom, y_nom / y_denom))
+
+        let cross_12 = x1.clone() * y2.clone() - y1.clone() * x2.clone();
+        let cross_34 = x3.clone() * y4.clone() - y3.clone() * x4.clone();
+
+        let x_nom = cross_12.clone() * (x3.clone() - x4.clone())
+            - (x1.clone() - x2.clone()) * cross_34.clone();
+        let y_nom = cross_12.clone() * (y3.clone() - y4.clone())
+            - (y1.clone() - y2.clone()) * cross_34.clone();
+        Some(((x_nom / denom.clone()).to_f64(), (y_nom / denom).to_f64()))
+    }
+
+    fn intersection_xy(&self, other: &Hailstone) -> Option<(f64, f64)> {
+        let p1 = &self.pos;
+        let p2 = &self.pos + &self.vel;
+        let p3 = &other.pos;
+        let p4 = &other.pos + &other.vel;
+        Self::_intersection((p1.x, p1.y), (p2.x, p2.y), (p3.x, p3.y), (p4.x, p4.y))
+    }
+
+    fn intersection_xz(&self, other: &Hailstone) -> Option<(f64, f64)> {
+        let p1 = &self.pos;
+        let p2 = &self.pos + &self.vel;
+        let p3 = &other.pos;
+        let p4 = &other.pos + &other.vel;
+        Self::_intersection((p1.x, p1.z), (p2.x, p2.z), (p3.x, p3.z), (p4.x, p4.z))
     }
 
     fn time_to_intersection(&self, pos: (f64, f64)) -> f64 {
         let delta_x = pos.0 - self.pos.x;
         delta_x / self.vel.x
     }
+
+    fn change_velocity(&self, vel: &Coord3) -> Hailstone {
+        Hailstone {
+            pos: self.pos.clone(),
+            vel: &self.vel - vel,
+        }
+    }
+}
+
+fn all_intersect_xy(hailstones: &Vec<Hailstone>) -> Option<(f64, f64)> {
+    let empty: (f64, f64) = (0.0, 0.0);
+    let mut intersection = empty.clone();
+
+    for i in 0..hailstones.len() {
+        let h1 = &hailstones[i];
+
+        for j in i + 1..hailstones.len() {
+            let h2 = &hailstones[j];
+
+            if let Some(intersection_xy) = h1.intersection_xy(h2) {
+                if intersection == empty {
+                    intersection = (intersection_xy.0, intersection_xy.1);
+                } else if intersection != intersection_xy {
+                    return None;
+                }
+            } else {
+                // Parallel lines in xy plane. Ignore.
+            }
+        }
+    }
+
+    Some((intersection.0 as f64, intersection.1 as f64))
+}
+
+fn all_intersect_xz(hailstones: &Vec<Hailstone>) -> Option<(f64, f64)> {
+    let empty = (0.0, 0.0);
+    let mut intersection = empty.clone();
+
+    for i in 0..hailstones.len() {
+        let h1 = &hailstones[i];
+
+        for j in i + 1..hailstones.len() {
+            let h2 = &hailstones[j];
+
+            if let Some(intersection_xz) = h1.intersection_xz(h2) {
+                if intersection == empty {
+                    intersection = intersection_xz;
+                } else if intersection != intersection_xz {
+                    return None;
+                }
+            } else {
+                // Parallel lines in xy plane. Ignore.
+            }
+        }
+    }
+
+    Some(intersection)
+}
+
+fn search_stone_parameters(
+    hailstones: &Vec<Hailstone>,
+    search_range: std::ops::Range<isize>,
+) -> Option<Hailstone> {
+    for x in search_range.start..=search_range.end {
+        for y in search_range.start..=search_range.end {
+            let mut vel = Coord3 {
+                x: x as f64,
+                y: y as f64,
+                z: 0.0,
+            };
+            let transposed: Vec<_> = hailstones.iter().map(|h| h.change_velocity(&vel)).collect();
+            if let Some(xy) = all_intersect_xy(&transposed) {
+                println!("Have first intersection at {}, {}", xy.0, xy.1);
+                for z in search_range.start..=search_range.end {
+                    vel.z = z as f64;
+                    let transposed: Vec<_> =
+                        hailstones.iter().map(|h| h.change_velocity(&vel)).collect();
+                    if let Some(xz) = all_intersect_xz(&transposed) {
+                        return Some(Hailstone {
+                            pos: Coord3 {
+                                x: xy.0,
+                                y: xy.1,
+                                z: xz.1,
+                            },
+                            vel,
+                        });
+                    }
+                }
+            }
+        }
+    }
+
+    None
 }
 
 fn part1(input: &str) -> Result<(), Error> {
@@ -123,7 +250,7 @@ fn part1(input: &str) -> Result<(), Error> {
         for j in i + 1..hailstones.len() {
             let h2 = &hailstones[j];
 
-            if let Some(intersection) = h1.intersection_flat(h2) {
+            if let Some(intersection) = h1.intersection_xy(h2) {
                 if is_inside(&intersection)
                     && h1.time_to_intersection(intersection) >= 0.0
                     && h2.time_to_intersection(intersection) >= 0.0
@@ -139,7 +266,20 @@ fn part1(input: &str) -> Result<(), Error> {
 }
 
 fn part2(input: &str) -> Result<(), Error> {
-    println!("Part 2: TBD");
+    let maybe_hailstones: Result<Vec<Hailstone>, Error> =
+        input.lines().map(|line| Hailstone::new(line)).collect();
+    let hailstones = maybe_hailstones?;
+
+    if let Some(stone) = search_stone_parameters(&hailstones, -500..500) {
+        println!("Stone: {} -> {}", stone.pos, stone.vel);
+        println!(
+            "Part 2: {}",
+            stone.pos.x.round() + stone.pos.y.round() + stone.pos.z.round()
+        );
+    } else {
+        println!("Part 2: no result")
+    }
+
     return Ok(());
 }
 
